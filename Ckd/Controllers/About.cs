38d9e7cb4e5719d1e72df5fa559ec345d0694bbc
ardliath <ardliath@users.noreply.github.com>
2021-03-25 17:24:54 +0000

    Added detail on about page

---
+++ Ckd/Controllers/About.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ckd.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Kidneys()
+        {
+            return View();
+        }
+
+        public IActionResult Ckd()
+        {
+            return View();
+        }
+
+        public IActionResult Haemodialysis()
+        {
+            return View();
+        }
+
+        public IActionResult PeritonealDialysis()
+        {
+            return View();
+        }
+
+        public IActionResult LivingDonor()
+        {
+            return View();
+        }
+
+        public IActionResult Medications()
+        {
+            return View();
+        }
+
+        public IActionResult ReceivingAKidney()
+        {
+            return View();
+        }
+
+        public IActionResult LivingWithNewKidney()
+        {
+            return View();
+        }
+
+        public IActionResult NgTube()
+        {
+            return View();
+        }
+
+        public IActionResult Peg()
+        {
+            return View();
+        }
+    }
+}
